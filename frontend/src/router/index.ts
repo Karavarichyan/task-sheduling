06c2962d1f54adf42fb90b31e364763bd7a51e4b
Narek <narekkaravarichyan1910@gmail.com>
2025-09-12 22:04:39 +0400

    add  routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,22 @@
+import Board from '@/views/Board.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import Login from '@/views/Login.vue'
+import Users from '@/views/Users.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { component: Login, path: '/login', name: 'login' },
+    {
+      component: Dashboard,
+      path: '/',
+      children: [
+        { component: Board, path: '', name: 'board' },
+        { component: Users, path: 'users', name: 'users' },
+      ],
+    },
+  ],
 })
 
 export default router
