c7d73c90ce6b3eadeb00cfbeac3a21afb51505b0
Narek <narekkaravarichyan1910@gmail.com>
2025-09-12 23:21:44 +0400

    add sidebar

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,11 +1,61 @@
 <script lang="ts" setup>
-import { useUserQuery } from '@/data/queries/user'
-import { RouterView } from 'vue-router'
+import SidebarUser from '@/components/sidebar-user/SidebarUser.vue'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from '@/components/ui/sidebar'
+import { KanbanIcon, UsersIcon } from 'lucide-vue-next'
+import { RouterLink, RouterView } from 'vue-router'
 
-const { data: user } = useUserQuery()
+const items = [
+  {
+    title: 'Board',
+    to: { name: 'board' },
+    icon: KanbanIcon,
+  },
+  {
+    title: 'Users',
+    to: { name: 'users' },
+    icon: UsersIcon,
+  },
+]
 </script>
 
 <template>
-  <div>Hello {{ user?.email }}</div>
-  <RouterView />
+  <SidebarProvider>
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton asChild>
+                  <RouterLink :to="item.to">
+                    <component :is="item.icon" />
+                    {{ item.title }}</RouterLink
+                  >
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarUser />
+      </SidebarFooter>
+    </Sidebar>
+    <SidebarInset>
+      <RouterView />
+    </SidebarInset>
+  </SidebarProvider>
 </template>
