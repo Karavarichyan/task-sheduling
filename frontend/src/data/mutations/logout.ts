5da9cb58ae402ffd79b5d9fe1465d1057c72b484
Narek <narekkaravarichyan1910@gmail.com>
2025-09-14 19:47:52 +0400

    fix logout

--- frontend/src/data/mutations/logout.ts
+++ frontend/src/data/mutations/logout.ts
@@ -1,20 +1,17 @@
 import { useMutation, useQueryClient } from '@tanstack/vue-query'
 import { useRouter } from 'vue-router'
 
-const logoutUser = async () => {
-  localStorage.removeItem('token')
-}
-
 export const useLogoutMutation = () => {
   const queryClient = useQueryClient()
   const router = useRouter()
 
   return useMutation({
-    mutationFn: logoutUser,
-    onSuccess: () => {
-      queryClient.clear()
-
-      router.push('/login')
+    mutationFn:async () =>{
+      localStorage.removeItem('token')
     },
+    onSuccess: () =>{
+      queryClient.clear()
+      router.push('/login')
+    }
   })
 }
